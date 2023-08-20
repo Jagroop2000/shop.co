9fe1520a2f6d5a4a68ca1925c4cc2f63de0b8b85
Jagroop2000 <jagroopofficial2000@gmail.com>
2023-08-20 17:39:17 +0530

    Header Implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
+import Header from "./components/header/Header"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl font-bold underline bg-red-500">
-      Hello world!
-    </h1>
+    <Header/>
     </>
   )
 }
